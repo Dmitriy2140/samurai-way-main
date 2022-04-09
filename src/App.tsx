31ad43cfa3db03e53c2a2833a1d97e268fb825e5
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-04-09 20:50:59 +0300

    11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe7XyJEKAiSyavfOhv8Oe80Eh2faL7zZMxJg&usqp=CAU'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Message</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Musik</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://www.sostav.ru/images/news/2019/06/20/d9eelmcf_md.jpg"/>
+                </div>
+                <div>
+                    Ava + discription
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     );
 }
 
