9eab64b7e40ad6f4a4c7be7521d2e9f75e358e60
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-04-11 17:55:07 +0300

    15 lesson

---
+++ src/Components/Profile/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://www.sostav.ru/images/news/2019/06/20/d9eelmcf_md.jpg"/>
+            </div>
+            <div>
+                Ava + discription
+            </div>
+            <MyPosts/>
+        </div>
+    )
+}
+
+export default Profile;
