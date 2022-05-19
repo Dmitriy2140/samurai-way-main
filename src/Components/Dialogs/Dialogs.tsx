d2ca09d7ec4fc911e7d5399b63d96ab92ec81e82
Dmitriy2140 <dimon_shkurat@mail.ru>
2022-05-19 19:22:45 +0300

    21 lesson

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 
- const Dialogs = () => {
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Slavnk
+                </div>
+                <div className={s.dialog}>
+                    Vasya
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+                <div className={s.dialog}>
+                    Kat
+                </div>
+                <div className={s.dialog}>
+                    Archi
+                </div>
+            </div>
+            <div className={s.messages}>
+<div className={s.message}>Hi</div>
+<div className={s.message}>How is your it-kamasutra?</div>
+<div className={s.message}>Yo</div>
+            </div>
         </div>
+
     )
 }
 
